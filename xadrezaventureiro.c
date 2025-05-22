68d9a3a9f085906f955bd5b6be9ad35d30c9f77a
agroon280 <caurrodrigo@gmail.com>
2025-05-22 20:38:27 +0000

    aventureiro

--- xadrezaventureiro.c
+++ xadrezaventureiro.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(){
-int escolha, i = 0;
+int escolha, i = 1;
 
 //irei fazer um menu com a escolha da peca que deseja movimentar
 printf("ESCOLHA A PECA QUE DESEJA MOVIMENTAR\n");
-printf("1-TORRE\n2-BISPO\n3-RAINHA\n");
+printf("1-TORRE\n2-BISPO\n3-RAINHA\n4-CAVALO\n");
 printf("DIGITE O NUMERO CORRESPONDENTE:\n");
 scanf("%d", &escolha);
 
@@ -29,6 +29,22 @@ while (i <= 8){
     i++;
 }
     break;
+    //implementei o movimento do cavalo usando if else, imprimindo com a condicao c igual a i
+    case 4:
+    for(i; i <= 1; i++){
+        int c = 1;
+while (c <= 3){
+    if(c==1){
+        printf("CAVALO MOVEU-SE PARA BAIXO\n");
+    }else if(c==2){
+        printf("CAVALO MOVEU-SE PARA BAIXO\n");
+    }else if(c==3){
+        printf("CAVALO MOVEU-SE PARA ESQUERDA\n");
+    }else;
+    c++;
+    }
+}
+break;
 default:
 printf("erro...invalido");
     break;
